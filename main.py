47a27883f00df921e3e88f53429168edf3875b50
Trevor Ablett <trevor.ablett@robotics.utias.utoronto.ca>
2020-02-11 14:01:14 -0500

    Add thing envs

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import torch
 import gym
 import argparse
 import os
+import sys
+import time
 
 import utils
 import TD3
 import OurDDPG
 import DDPG
 
+from manipulator_learning.sim.envs.thing_reaching import ThingReachingXYState, ThingReachingXYImage
+from manipulator_learning.sim.envs.thing_pushing import ThingPushingXYState, ThingPushingXYImage
+
+# corrective interaction discriminator imports
+# from .discriminator_interaction.bc_pt import behavior_clone
+
 
 # Runs policy for X episodes and returns average reward
 # A fixed seed is used for the eval environment
 def eval_policy(policy, env_name, seed, eval_episodes=10):
-	eval_env = gym.make(env_name)
+	if 'Thing' in env_name:  # todo add thing envs to actual gym list
+		eval_env = getattr(sys.modules[__name__], env_name)()
+	else:
+		eval_env = gym.make(env_name)
 	eval_env.seed(seed + 100)
 
 	avg_reward = 0.
@@ -38,8 +49,8 @@ if __name__ == "__main__":
 	parser.add_argument("--policy", default="TD3")                  # Policy name (TD3, DDPG or OurDDPG)
 	parser.add_argument("--env", default="HalfCheetah-v2")          # OpenAI gym environment name
 	parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
-	parser.add_argument("--start_timesteps", default=1e4, type=int) # Time steps initial random policy is used
-	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
+	parser.add_argument("--start_timesteps", default=1e3, type=int) # Time steps initial random policy is used
+	parser.add_argument("--eval_freq", default=5e2, type=int)       # How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=1e6, type=int)   # Max time steps to run environment
 	parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
@@ -50,7 +61,16 @@ if __name__ == "__main__":
 	parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
 	parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
 	parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
+	parser.add_argument("--render", action="store_true")            # Render env on rollouts
 	args = parser.parse_args()
+
+	# discriminator interaction arguments  #todo make this a yaml or similar
+	disc_int_args = dict(
+		discrim_reward=True,
+		allow_interventions=False,
+		pre_train=True,
+		expert_data_file="./expert_data/ThingReachingXYState.npy"
+	)
 
 	file_name = f"{args.policy}_{args.env}_{args.seed}"
 	print("---------------------------------------")
@@ -63,7 +83,10 @@ if __name__ == "__main__":
 	if args.save_model and not os.path.exists("./models"):
 		os.makedirs("./models")
 
-	env = gym.make(args.env)
+	if 'Thing' in args.env:  # todo add thing envs to actual gym list
+		env = getattr(sys.modules[__name__], args.env)()
+	else:
+		env = gym.make(args.env)
 
 	# Set seeds
 	env.seed(args.seed)
@@ -93,6 +116,9 @@ if __name__ == "__main__":
 		policy = OurDDPG.DDPG(**kwargs)
 	elif args.policy == "DDPG":
 		policy = DDPG.DDPG(**kwargs)
+
+	# if disc_int_args['pre_train']:
+	# 	policy.actor = behavior_clone(policy, )
 
 	if args.load_model != "":
 		policy_file = file_name if args.load_model == "default" else args.load_model
@@ -131,9 +157,14 @@ if __name__ == "__main__":
 		state = next_state
 		episode_reward += reward
 
+		if args.render:
+			env.render()
+
 		# Train agent after collecting sufficient data
 		if t >= args.start_timesteps:
+			model_update_start = time.time()
 			policy.train(replay_buffer, args.batch_size)
+			# print('model update time: ', time.time() - model_update_start)
 
 		if done: 
 			# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
